cad314501fcb1176950eab35f148c84c3c126e87
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2024-05-01 14:38:41 -0300

    Create Home screen
    Notes:
    * MovieItemViewModel is not created like a ViewModel, it isn't saved to
    the store, the reason for that is that each item need a separate
    ViewModel instance, which can't be done using the store
    * In MovieItem.kt, AsyncImage's model has to be a String, it cannot be
    a URL object; although it's allegedly supported, it fails to load the
    image, using a String is the workaround

--- app/src/main/java/tenfen/rodolfo/moviesapp/ui/theme/Theme.kt
+++ app/src/main/java/tenfen/rodolfo/moviesapp/ui/theme/Theme.kt
@@ -1,71 +1,41 @@
 package tenfen.rodolfo.moviesapp.ui.theme
 
 import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.TextStyle
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme =
+private val moviesColorScheme =
     darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
-    )
-
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
+        primary = primary,
+        secondary = secondary,
+        tertiary = tertiary,
+        background = screenBackground,
+        surface = listItemBackground
     )
 
 @Composable
-fun MoviesAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit,
-) {
-    val colorScheme =
-        when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
-        }
+fun MoviesAppTheme(content: @Composable () -> Unit) {
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = moviesColorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = moviesColorScheme,
         typography = Typography,
-        content = content,
+        content = {
+            ProvideTextStyle(value = TextStyle(color = primaryText), content = content)
+        }
     )
 }
