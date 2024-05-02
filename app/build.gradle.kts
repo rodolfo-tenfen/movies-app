68c64e0350e5f63323b3e94d3f396bfb4b3084b2
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2024-05-01 21:14:32 -0300

    Change plugin usage to use Version Catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jlleitschuh.gradle.ktlint")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ktlint.gradle)
 }
 
 android {
