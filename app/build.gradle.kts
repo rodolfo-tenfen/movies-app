b9f3bfd28f98d89de302cb05888fd1c18c4ac642
Rodolfo Tenfen <rodolfo.tenfen@quintoandar.com.br>
2026-03-22 13:58:21 -0300

    Add Kotlin Compose plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ktlint.gradle)
     alias(libs.plugins.ksp)
 }
