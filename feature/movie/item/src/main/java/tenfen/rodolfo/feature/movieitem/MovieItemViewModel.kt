2aa44684fee2dcb2939a2b65476442d688b10b38
Rodolfo Tenfen <rodolfo.tenfen@quintoandar.com.br>
2026-03-29 02:15:52 -0300

    Delete MovieItemViewModel

--- feature/movie/item/src/main/java/tenfen/rodolfo/feature/movieitem/MovieItemViewModel.kt
+++
@@ -1,9 +0,0 @@
-package tenfen.rodolfo.feature.movieitem
-
-import androidx.lifecycle.ViewModel
-import tenfen.rodolfo.domain.movie.entity.Movie
-
-class MovieItemViewModel(val movie: Movie) : ViewModel() {
-
-    val isOriginalTitleVisible get() = movie.title != movie.originalTitle
-}
