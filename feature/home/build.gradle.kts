efa2102be7d5cda97995d9bfde18158d310cdcdd
Rodolfo Tenfen <rodolfo.tenfen@quintoandar.com.br>
2026-04-06 01:45:12 -0300

    Rename material dependency
    From material3 to material

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
