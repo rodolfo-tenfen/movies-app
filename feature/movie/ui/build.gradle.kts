034e27d727a9a37a1dfd3c5ed284b36cdd941ec3
Rodolfo Tenfen <rodolfo.tenfen@quintoandar.com.br>
2026-04-06 01:47:36 -0300

    Remove unused dependencies from :movie:ui and :movie:ui:item

--- feature/movie/ui/build.gradle.kts
+++ feature/movie/ui/build.gradle.kts
@@ -50,13 +50,9 @@ dependencies {
     api(project(":feature:movie:model"))
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.runtime.livedata)
     implementation(libs.androidx.compose.material)
     implementation(libs.coil)
     implementation(libs.coil.compose)
--- feature/movie/ui/item/build.gradle.kts
+++ feature/movie/ui/item/build.gradle.kts
@@ -56,8 +56,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.runtime.livedata)
     implementation(libs.androidx.compose.material)
     implementation(libs.coil)
     implementation(libs.coil.compose)
